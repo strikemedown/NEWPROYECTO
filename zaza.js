1304f126104c45e6f1ff455dc635e54d04ea57b5
strikemedown <143925923+strikemedown@users.noreply.github.com>
2023-12-03 17:56:38 -0500

    Add files via upload

--- zaza.js
+++ zaza.js
@@ -1,20 +1,53 @@
 window.addEventListener("load",THESTART);
 
 function THESTART(){
-	document.getElementById("LNAME").disabled= true;
-	document.getElementById("GL").disabled= true;
-	document.getElementById("SLC").disabled= true;
-	document.getElementById("EMAIL").disabled= true;
-	document.getElementById("FNAME").addEventListener("keypress",NOMBRE);
-	document.getElementById("LNAME").addEventListener("keypress",APELLIDO)
-	document.getElementById("SLC").addEventListener("keypress",PEQUENACOMUNIDADDEAPRENDIZAJE)
-	document.getElementById("GL").addEventListener("keypress",GRADO)
-	document.getElementById("FNAME").addEventListener("keypress",CORREO)
-	
+	document.getElementById("APELLIDO").disabled= true;
+	document.getElementById("SEGUNDONOMBRE").disabled= true;
+	document.getElementById("PEQUENACOMUNIDADDEAPRENDIZAJE").disabled= true;
+	document.getElementById("NIVELDEGRADO").disabled= true;
+	document.getElementById("DIRECCIONDECORREOELECTRONICO").disabled= true;
+	document.getElementById("ENTREGAR").disabled= true;
+	document.getElementById("NOMBRE").addEventListener("keypress",NOMBRE);
+	document.getElementById("APELLIDO").addEventListener("keypress",APELLIDO);
+	document.getElementById("PEQUENACOMUNIDADDEAPRENDIZAJE").addEventListener("keypress",PEQUENACOMUNIDADDEAPRENDIZAJE);
+	document.getElementById("NIVELDEGRADO").addEventListener("keypress",NIVELDEGRADO);
+	document.getElementById("DIRECCIONDECORREOELECTRONICO").addEventListener("keypress",DIRECCIONDECORREOELECTRONICO);
+	document.getElementById("ENTREGAR").addEventListener("click", ENTREGAR)
 }
 
 function NOMBRE(){
-	document.getElementById("LNAME").disabled= false
+	document.getElementById("APELLIDO").disabled= false;
+	document.getElementById("SEGUNDONOMBRE").disabled= false;
 }
 
-function 
+function APELLIDO(){
+	document.getElementById("PEQUENACOMUNIDADDEAPRENDIZAJE").disabled= false;
+}
+
+function PEQUENACOMUNIDADDEAPRENDIZAJE(){
+	document.getElementById("NIVELDEGRADO").disabled= false;
+}
+
+function NIVELDEGRADO(){
+	document.getElementById("DIRECCIONDECORREOELECTRONICO").disabled= false;
+}
+
+function DIRECCIONDECORREOELECTRONICO(){
+	document.getElementById("ENTREGAR").disabled= false;
+}
+
+function ENTREGAR(){
+	NOMBRE= document.getElementById("NOMBRE").value;
+	SEGUNDONOMBRE= document.getElementById("SEGUNDONOMBRE").value;
+	APELLIDO= document.getElementById("APELLIDO").value;
+	PEQUENACOMUNIDADDEAPRENDIZAJE= document.getElementById("PEQUENACOMUNIDADDEAPRENDIZAJE").value;
+	NIVELDEGRADO= document.getElementById("NIVELDEGRADO").value;
+	DIRECCIONDECORREOELECTRONICO= document.getElementById("DIRECCIONDECORREOELECTRONICO").value;
+	
+	document.getElementById("NOMBRE1").textContent= NOMBRE;
+	document.getElementById("SEGUNDONOMBRE1").textContent= SEGUNDONOMBRE+".";
+	document.getElementById("APELLIDO1").textContent= APELLIDO;
+	document.getElementById("NIVELDEGRADO1").textContent= NIVELDEGRADO+"th";
+	document.getElementById("DIRECCIONDECORREOELECTRONICO1").textContent= DIRECCIONDECORREOELECTRONICO;
+	document.getElementById("PEQUENACOMUNIDADDEAPRENDIZAJE1").textContent= PEQUENACOMUNIDADDEAPRENDIZAJE;
+}

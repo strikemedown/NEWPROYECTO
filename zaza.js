73b277cdee0ae86064869db1d3599b9d9edc5561
strikemedown <143925923+strikemedown@users.noreply.github.com>
2023-12-11 07:07:26 -0500

    Add files via upload

--- zaza.js
+++ zaza.js
@@ -1,6 +1,6 @@
-window.addEventListener("load",THESTART);
+window.addEventListener("load",disable);
 
-function THESTART(){
+function disable(){
 	document.getElementById("LASTNAME").disabled= true;
 	document.getElementById("MIDDLENAME").disabled= true;
 	document.getElementById("ID").disabled= true;
@@ -25,11 +25,14 @@ function THESTART(){
 	document.getElementById("GRADE7").disabled= true;
 	document.getElementById("GRADE8").disabled= true;
 	document.getElementById("SUBMIT").disabled= true;
+	THESTART()
+}
+function THESTART(){
 	document.getElementById("NAME").addEventListener("keypress",NAME);
 	document.getElementById("LASTNAME").addEventListener("keypress",LASTNAME);
 	document.getElementById("ID").addEventListener("keypress",ID);
-	document.getElementById("SLC").addEventListener("keypress",SLC);
-	document.getElementById("LEVELGRADE").addEventListener("keypress",LEVELGRADE);
+	document.getElementById("SLC").addEventListener("click",SLC);
+	document.getElementById("LEVELGRADE").addEventListener("click",LEVELGRADE);
 	document.getElementById("EMAILADDRESS").addEventListener("keypress",EMAILADDRESS);
 	document.getElementById("COURSE1").addEventListener("keypress",COURSE1);
 	document.getElementById("COURSE2").addEventListener("keypress",COURSE2);
@@ -151,14 +154,64 @@ function GRADE7(){
 function GRADE8(){
 	document.getElementById("SUBMIT").disabled= false;
 }
-
+function help(){
+	THESTART()
+}
 function SUBMIT(){
+	var grade= document.getElementById("LEVELGRADE").selectedIndex
+	
+	var slc= document.getElementById("SLC").selectedIndex
+	switch(slc){
+		case 1: 
+			slc="HSSE";
+			break;
+		case 2:
+			slc="Pre-Med";
+			break;
+		case 3:
+			slc="Theatre";
+			break;
+		case 4:
+			slc="Humanities";
+			break;
+		case 5:
+			slc="Law";
+			break;
+		case 6:
+			slc="TOT";
+			break;
+		case 7:
+			slc="Global Citizens";
+			break;
+		default:
+			slc="N/A"
+			alert("Not a SLC.")
+	}
+	
+	switch(grade){
+		case 1: 
+			grade="9";
+			break;
+		case 2:
+			grade="10";
+			break;
+		case 3:
+			grade="11";
+			break;
+		case 4:
+			grade="12";
+			break;
+		default:
+			grade="N/A"
+			alert("Not a grade level.");
+	}
+	
 	document.getElementById("NAME1").textContent= document.getElementById("NAME").value;
 	document.getElementById("MIDDLENAME1").textContent= document.getElementById("MIDDLENAME").value+".";
 	document.getElementById("LASTNAME1").textContent= document.getElementById("LASTNAME").value;
 	document.getElementById("ID1").textContent= document.getElementById("ID").value;
-	document.getElementById("LEVELGRADE1").textContent= document.getElementById("LEVELGRADE").value;
-	document.getElementById("SLC1").textContent= document.getElementById("SLC").value;
+	document.getElementById("LEVELGRADE1").textContent= grade;
+	document.getElementById("SLC1").textContent= slc;
 	document.getElementById("EMAILADDRESS1").textContent= document.getElementById("EMAILADDRESS").value;
 	document.getElementById("COURSE11").textContent= document.getElementById("COURSE1").value;
 	document.getElementById("COURSE22").textContent= document.getElementById("COURSE2").value;
@@ -178,328 +231,336 @@ function SUBMIT(){
 	var th7= document.getElementById("GRADE7").value;
 	var th8= document.getElementById("GRADE8").value;
 	
-	document.getElementById("1st").textContent= st
-	document.getElementById("2nd").textContent= nd
-	document.getElementById("3rd").textContent= rd
-	document.getElementById("4th").textContent= th4
-	document.getElementById("5th").textContent= th5
-	document.getElementById("6th").textContent= th6
-	document.getElementById("7th").textContent= th7
-	document.getElementById("8th").textContent= th8
+	document.getElementById("1st").textContent= st;
+	document.getElementById("2nd").textContent= nd;
+	document.getElementById("3rd").textContent= rd;
+	document.getElementById("4th").textContent= th4;
+	document.getElementById("5th").textContent= th5;
+	document.getElementById("6th").textContent= th6;
+	document.getElementById("7th").textContent= th7;
+	document.getElementById("8th").textContent= th8;
 	
-	if (parseInt(st)>=97){
-		var LETTER="A+";
+	switch(true){
+	    case parseInt(st) >= 97:
+	        LETTER = "A+";
+	        break;
+	    case parseInt(st) >= 93:
+	        LETTER = "A";
+	        break;
+	    case parseInt(st) >= 90:
+	        LETTER = "A-";
+	        break;
+	    case parseInt(st) >= 87:
+	        LETTER = "B+";
+	        break;
+	    case parseInt(st) >= 83:
+	        LETTER = "B";
+	        break;
+	    case parseInt(st) >= 80:
+	        LETTER = "B-";
+	        break;
+	    case parseInt(st) >= 77:
+	        LETTER = "C+";
+	        break;
+	    case parseInt(st) >= 73:
+	        LETTER = "C";
+	        break;
+	    case parseInt(st) >= 70:
+	        LETTER = "C-";
+	        break;
+	    case parseInt(st) >= 67:
+	        LETTER = "D+";
+	        break;
+	    case parseInt(st) >= 65:
+	        LETTER = "D";
+	        break;
+	    default:
+	        LETTER = "F";
 	}
-	else if (parseInt(st)>=93){
-		var LETTER="A";
-	}
-	else if (parseInt(st)>=90){
-		var LETTER="A-";
-	}
-	else if (parseInt(st)>=87){
-		var LETTER="B+";
-	}
-	else if (parseInt(st)>=83){
-		var LETTER="B";
-	}
-	else if (parseInt(st)>=80){
-		var LETTER="B-";
-	}
-	else if (parseInt(st)>=77){
-		var LETTER="C+";
-	}
-	else if (parseInt(st)>=73){
-		var LETTER="C";
-	}
-	else if (parseInt(st)>=70){
-		var LETTER="C-";
-	}
-	else if (parseInt(st)>=67){
-		var LETTER="D+";
-	}
-	else if (parseInt(st)>=65){
-		var LETTER="D";
-	}
-	else if (parseInt(st)<65){
-		var LETTER="F";
-	};
-	document.getElementById("lg1").textContent= LETTER;
+	document.getElementById("lg1").textContent = LETTER;
 	
-	if (parseInt(nd)>=97){
-		var LETTER="A+";
+	switch(true){
+		case parseInt(nd) >= 97:
+	        LETTER = "A+";
+	        break;
+	    case parseInt(nd) >= 93:
+	        LETTER = "A";
+	        break;
+	    case parseInt(nd) >= 90:
+	        LETTER = "A-";
+	        break;
+	    case parseInt(nd) >= 87:
+	        LETTER = "B+";
+	        break;
+	    case parseInt(nd) >= 83:
+	        LETTER = "B";
+	        break;
+	    case parseInt(nd) >= 80:
+	        LETTER = "B-";
+	        break;
+	    case parseInt(nd) >= 77:
+	        LETTER = "C+";
+	        break;
+	    case parseInt(nd) >= 73:
+	        LETTER = "C";
+	        break;
+	    case parseInt(nd) >= 70:
+	        LETTER = "C-";
+	        break;
+	    case parseInt(nd) >= 67:
+	        LETTER = "D+";
+	        break;
+	    case parseInt(nd) >= 65:
+	        LETTER = "D";
+	        break;
+	    default:
+			LETTER = "F";
 	}
-	else if (parseInt(nd)>=93){
-		var LETTER="A";
-	}
-	else if (parseInt(nd)>=90){
-		var LETTER="A-";
-	}
-	else if (parseInt(nd)>=87){
-		var LETTER="B+";
-	}
-	else if (parseInt(nd)>=83){
-		var LETTER="B";
-	}
-	else if (parseInt(nd)>=80){
-		var LETTER="B-";
-	}
-	else if (parseInt(nd)>=77){
-		var LETTER="C+";
-	}
-	else if (parseInt(nd)>=73){
-		var LETTER="C";
-	}
-	else if (parseInt(nd)>=70){
-		var LETTER="C-";
-	}
-	else if (parseInt(nd)>=67){
-		var LETTER="D+";
-	}
-	else if (parseInt(nd)>=65){
-		var LETTER="D";
-	}
-	else if (parseInt(nd)<65){
-		var LETTER="F";
-	}
-	document.getElementById("lg2").textContent= LETTER
+	document.getElementById("lg2").textContent = LETTER;
 	
-	if (parseInt(rd)>=97){
-		var LETTER="A+"
+	switch(true){
+		case parseInt(rd) >= 97:
+	        LETTER = "A+";
+	        break;
+	    case parseInt(rd) >= 93:
+	        LETTER = "A";
+	        break;
+	    case parseInt(rd) >= 90:
+	        LETTER = "A-";
+	        break;
+	    case parseInt(rd) >= 87:
+	        LETTER = "B+";
+	        break;
+	    case parseInt(rd) >= 83:
+	        LETTER = "B";
+	        break;
+	    case parseInt(rd) >= 80:
+	        LETTER = "B-";
+	        break;
+	    case parseInt(rd) >= 77:
+	        LETTER = "C+";
+	        break;
+	    case parseInt(rd) >= 73:
+	        LETTER = "C";
+	        break;
+	    case parseInt(rd) >= 70:
+	        LETTER = "C-";
+	        break;
+	    case parseInt(rd) >= 67:
+	        LETTER = "D+";
+	        break;
+	    case parseInt(rd) >= 65:
+	        LETTER = "D";
+	        break;
+	    default:
+	        LETTER = "F";
 	}
-	else if (parseInt(rd)>=93){
-		var LETTER="A"
-	}
-	else if (parseInt(rd)>=90){
-		var LETTER="A-"
-	}
-	else if (parseInt(rd)>=87){
-		var LETTER="B+"
-	}
-	else if (parseInt(rd)>=83){
-		var LETTER="B"
-	}
-	else if (parseInt(rd)>=80){
-		var LETTER="B-"
-	}
-	else if (parseInt(rd)>=77){
-		var LETTER="C+"
-	}
-	else if (parseInt(rd)>=73){
-		var LETTER="C"
-	}
-	else if (parseInt(rd)>=70){
-		var LETTER="C-"
-	}
-	else if (parseInt(rd)>=67){
-		var LETTER="D+"
-	}
-	else if (parseInt(rd)>=65){
-		var LETTER="D"
-	}
-	else if (parseInt(rd)<65){
-		var LETTER="F"
-	}
-	document.getElementById("lg3").textContent= LETTER
+	document.getElementById("lg3").textContent = LETTER;
 	
-	if (parseInt(th4)>=97){
-		var LETTER="A+"
-	}
-	else if (parseInt(th4)>=93){
-		var LETTER="A"
-	}
-	else if (parseInt(th4)>=90){
-		var LETTER="A-"
-	}
-	else if (parseInt(th4)>=87){
-		var LETTER="B+"
-	}
-	else if (parseInt(th4)>=83){
-		var LETTER="B"
-	}
-	else if (parseInt(th4)>=80){
-		var LETTER="B-"
-	}
-	else if (parseInt(th4)>=77){
-		var LETTER="C+"
-	}
-	else if (parseInt(th4)>=73){
-		var LETTER="C"
-	}
-	else if (parseInt(th4)>=70){
-		var LETTER="C-"
-	}
-	else if (parseInt(th4)>=67){
-		var LETTER="D+"
-	}
-	else if (parseInt(th4)>=65){
-		var LETTER="D"
-	}
-	else if (parseInt(th4)<65){
-		var LETTER="F"
-	}
-	document.getElementById("lg4").textContent= LETTER
+	switch(true){
+		case parseInt(th4) >= 97:
+	        LETTER = "A+";
+	        break;
+	    case parseInt(th4) >= 93:
+	        LETTER = "A";
+	        break;
+	    case parseInt(th4) >= 90:
+	        LETTER = "A-";
+	        break;
+	    case parseInt(th4) >= 87:
+	        LETTER = "B+";
+	        break;
+	    case parseInt(th4) >= 83:
+	        LETTER = "B";
+	        break;
+	    case parseInt(th4) >= 80:
+	        LETTER = "B-";
+	        break;
+	    case parseInt(th4) >= 77:
+	        LETTER = "C+";
+	        break;
+	    case parseInt(th4) >= 73:
+	        LETTER = "C";
+	        break;
+	    case parseInt(th4) >= 70:
+	        LETTER = "C-";
+	        break;
+	    case parseInt(th4) >= 67:
+	        LETTER = "D+";
+	        break;
+	    case parseInt(th4) >= 65:
+	        LETTER = "D";
+	        break;
+	    default:
+	        LETTER = "F";
+	 }
+	 document.getElementById("lg4").textContent = LETTER;
 	
-	if (parseInt(th5)>=97){
-		var LETTER="A+"
-	}
-	else if (parseInt(th5)>=93){
-		var LETTER="A"
-	}
-	else if (parseInt(th5)>=90){
-		var LETTER="A-"
-	}
-	else if (parseInt(th5)>=87){
-		var LETTER="B+"
-	}
-	else if (parseInt(th5)>=83){
-		var LETTER="B"
-	}
-	else if (parseInt(th5)>=80){
-		var LETTER="B-"
-	}
-	else if (parseInt(th5)>=77){
-		var LETTER="C+"
-	}
-	else if (parseInt(th5)>=73){
-		var LETTER="C"
-	}
-	else if (parseInt(th5)>=70){
-		var LETTER="C-"
-	}
-	else if (parseInt(th5)>=67){
-		var LETTER="D+"
-	}
-	else if (parseInt(th5)>=65){
-		var LETTER="D"
-	}
-	else if (parseInt(th5)<65){
-		var LETTER="F"
-	}
-	document.getElementById("lg5").textContent= LETTER
+	 switch(true){
+		 case parseInt(th5) >= 97:
+	         LETTER = "A+";
+	         break;
+	     case parseInt(th5) >= 93:
+	        LETTER = "A";
+	        break;
+	     case parseInt(th5) >= 90:
+	         LETTER = "A-";
+	         break;
+	     case parseInt(th5) >= 87:
+	         LETTER = "B+";
+	         break;
+	     case parseInt(th5) >= 83:
+	         LETTER = "B";
+	         break;
+	     case parseInt(th5) >= 80:
+	         LETTER = "B-";
+	         break;
+	     case parseInt(th5) >= 77:
+	         LETTER = "C+";
+	         break;
+	     case parseInt(th5) >= 73:
+	         LETTER = "C";
+	         break;
+	     case parseInt(th5) >= 70:
+	         LETTER = "C-";
+	         break;
+	     case parseInt(th5) >= 67:
+	         LETTER = "D+";
+	         break;
+	     case parseInt(th5) >= 65:
+	         LETTER = "D";
+	         break;
+	     default:
+	         LETTER = "F";
+	 }
+	 document.getElementById("lg5").textContent = LETTER;
 	
-	if (parseInt(th6)>=97){
-		var LETTER="A+"
-	}
-	else if (parseInt(th6)>=93){
-		var LETTER="A"
-	}
-	else if (parseInt(th6)>=90){
-		var LETTER="A-"
-	}
-	else if (parseInt(th6)>=87){
-		var LETTER="B+"
-	}
-	else if (parseInt(th6)>=83){
-		var LETTER="B"
-	}
-	else if (parseInt(th6)>=80){
-		var LETTER="B-"
-	}
-	else if (parseInt(th6)>=77){
-		var LETTER="C+"
-	}
-	else if (parseInt(th6)>=73){
-		var LETTER="C"
-	}
-	else if (parseInt(th6)>=70){
-		var LETTER="C-"
-	}
-	else if (parseInt(th6)>=67){
-		var LETTER="D+"
-	}
-	else if (parseInt(th6)>=65){
-		var LETTER="D"
-	}
-	else if (parseInt(th6)<65){
-		var LETTER="F"
-	}
-	document.getElementById("lg6").textContent= LETTER
+	 switch (true) {
+	     case parseInt(th6) >= 97:
+	         LETTER = "A+";
+	         break;
+	     case parseInt(th6) >= 93:
+	         LETTER = "A";
+	         break;
+	     case parseInt(th6) >= 90:
+	         LETTER = "A-";
+	         break;
+	     case parseInt(th6) >= 87:
+	         LETTER = "B+";
+	         break;
+	     case parseInt(th6) >= 83:
+	         LETTER = "B";
+	         break;
+	     case parseInt(th6) >= 80:
+	         LETTER = "B-";
+	         break;
+	     case parseInt(th6) >= 77:
+	         LETTER = "C+";
+	         break;
+	     case parseInt(th6) >= 73:
+	         LETTER = "C";
+	         break;
+	     case parseInt(th6) >= 70:
+	         LETTER = "C-";
+	         break;
+	     case parseInt(th6) >= 67:
+	         LETTER = "D+";
+	         break;
+	     case parseInt(th6) >= 65:
+	         LETTER = "D";
+	         break;
+	     default:
+	         LETTER = "F";
+	 }
+	 document.getElementById("lg6").textContent = LETTER;
 	
-	if (parseInt(th7)>=97){
-		var LETTER="A+"
-	}
-	else if (parseInt(th7)>=93){
-		var LETTER="A"
-	}
-	else if (parseInt(th7)>=90){
-		var LETTER="A-"
-	}
-	else if (parseInt(th7)>=87){
-		var LETTER="B+"
-	}
-	else if (parseInt(th7)>=83){
-		var LETTER="B"
-	}
-	else if (parseInt(th7)>=80){
-		var LETTER="B-"
-	}
-	else if (parseInt(th7)>=77){
-		var LETTER="C+"
-	}
-	else if (parseInt(th7)>=73){
-		var LETTER="C"
-	}
-	else if (parseInt(th7)>=70){
-		var LETTER="C-"
-	}
-	else if (parseInt(th7)>=67){
-		var LETTER="D+"
-	}
-	else if (parseInt(th7)>=65){
-		var LETTER="D"
-	}
-	else if (parseInt(th7)<65){
-		var LETTER="F"
-	}
-	document.getElementById("lg7").textContent= LETTER
+	 switch (true) {
+	     case parseInt(th7) >= 97:
+	         LETTER = "A+";
+	         break;
+	     case parseInt(th7) >= 93:
+	         LETTER = "A";
+	         break;
+	     case parseInt(th7) >= 90:
+	         LETTER = "A-";
+	         break;
+	     case parseInt(th7) >= 87:
+	         LETTER = "B+";
+	         break;
+	     case parseInt(th7) >= 83:
+	         LETTER = "B";
+	         break;
+	     case parseInt(th7) >= 80:
+	         LETTER = "B-";
+	         break;
+	     case parseInt(th7) >= 77:
+	         LETTER = "C+";
+	         break;
+	     case parseInt(th7) >= 73:
+	         LETTER = "C";
+	         break;
+	     case parseInt(th7) >= 70:
+	         LETTER = "C-";
+	         break;
+	     case parseInt(th7) >= 67:
+	         LETTER = "D+";
+	         break;
+	     case parseInt(th7) >= 65:
+	         LETTER = "D";
+	         break;
+	     default:
+	         LETTER = "F";
+	 }
+	 document.getElementById("lg7").textContent = LETTER;
 	
-	if (parseInt(th8)>=97){
-		var LETTER="A+"
-	}
-	else if (parseInt(th8)>=93){
-		var LETTER="A"
-	}
-	else if (parseInt(th8)>=90){
-		var LETTER="A-"
-	}
-	else if (parseInt(th8)>=87){
-		var LETTER="B+"
-	}
-	else if (parseInt(th8)>=83){
-		var LETTER="B"
-	}
-	else if (parseInt(th8)>=80){
-		var LETTER="B-"
-	}
-	else if (parseInt(th8)>=77){
-		var LETTER="C+";
-	}
-	else if (parseInt(th8)>=73){
-		var LETTER="C";
-	}
-	else if (parseInt(th8)>=70){
-		var LETTER="C-";
-	}
-	else if (parseInt(th8)>=67){
-		var LETTER="D+";
-	}
-	else if (parseInt(th8)>=65){
-		var LETTER="D";
-	}
-	else if (parseInt(th8)<65){
-		var LETTER="F";
-	};
-	document.getElementById("lg8").textContent= LETTER;
+	 switch (true) {
+	     case parseInt(th8) >= 97:
+	         LETTER = "A+";
+	         break;
+	     case parseInt(th8) >= 93:
+	         LETTER = "A";
+	         break;
+	     case parseInt(th8) >= 90:
+	         LETTER = "A-";
+	         break;
+	     case parseInt(th8) >= 87:
+	         LETTER = "B+";
+	         break;
+	     case parseInt(th8) >= 83:
+	         LETTER = "B";
+	         break;
+	     case parseInt(th8) >= 80:
+	         LETTER = "B-";
+	         break;
+	     case parseInt(th8) >= 77:
+	         LETTER = "C+";
+	         break;
+	     case parseInt(th8) >= 73:
+	         LETTER = "C";
+	         break;
+	     case parseInt(th8) >= 70:
+	         LETTER = "C-";
+	         break;
+	     case parseInt(th8) >= 67:
+	         LETTER = "D+";
+	         break;
+	     case parseInt(th8) >= 65:
+	         LETTER = "D";
+	         break;
+	     default:
+	         LETTER = "F";
+	 }
+	 document.getElementById("lg8").textContent = LETTER;
 	
-	var GPA1=st/20-1;
-	var GPA2=nd/20-1;
-	var GPA3=rd/20-1;
-	var GPA4=th4/20-1;
-	var GPA5=th5/20-1;
-	var GPA6=th6/20-1;
-	var GPA7=th7/20-1;
-	var GPA8=th8/20-1;
+	var GPA1=st/100*4;
+	var GPA2=nd/100*4;
+	var GPA3=rd/100*4;
+	var GPA4=th4/100*4;
+	var GPA5=th5/100*4;
+	var GPA6=th6/100*4;
+	var GPA7=th7/100*4;
+	var GPA8=th8/100*4;
 	
 	THEFINALE=(GPA1+GPA2+GPA3+GPA4+GPA5+GPA6+GPA7+GPA8)/8;
-	document.getElementById("GPA").textContent= THEFINALE
+	document.getElementById("GPA").textContent= THEFINALE;
 }
